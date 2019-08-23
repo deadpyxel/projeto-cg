5a5b680d31f437f443e06266211de3ee5853594e
deadpyxel <ghost0092@gmail.com>
2019-08-23 14:45:20 -0300

    feat: Add hls to rbg conversion

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 
 RBGMAX = 255
-HLSMAX = [360, 100., 100.]
+HLSMAX = [360, 1., 1.]
 
 # https://support.microsoft.com/pt-br/help/29240/how-to-converting-colors-between-rgb-and-hls-hbs
 
@@ -21,12 +21,6 @@ def draw_line(p1, p2):
             x = round((y - b) / a)
             points.append([x, y])
     return points
-
-
-Point = namedtuple('Point', 'x y')
-p2 = Point(100, 100)
-p1 = Point(120, 600)
-print(draw_line(p1, p2))
 
 
 def rbg_to_hls(rbg_values: dict):
@@ -56,8 +50,32 @@ def rbg_to_hls(rbg_values: dict):
     return {'hue': hue, 'lightness': lightness, 'saturation': saturation}
 
 
-def hls_to_rbg():
-    pass
+def hls_to_rbg(hls_values: dict):
+    # https://www.rapidtables.com/convert/color/hsl-to-rgb.html
+    c = (1 - abs(2 * hls_values['lightness'] - 1) * hls_values['saturation'])
+    x = c * (1 - abs((hls_values['hue'] / 60.) % 2 - 1))
+    m = hls_values['lightness'] - c / 2.
+
+    _r, _g, _b = 0, 0, 0
+    if 0. < hls_values['hue'] < 60.:
+        _r, _g, _b = c, x, 0
+    elif 60. <= hls_values['hue'] < 120.:
+        _r, _g, _b = x, c, 0
+    elif 120. <= hls_values['hue'] < 180.:
+        _r, _g, _b = 0, c, x
+    elif 180. <= hls_values['hue'] < 240.:
+        _r, _g, _b = 0, x, c
+    elif 240. <= hls_values['hue'] < 300.:
+        _r, _g, _b = x, 0, c
+    elif 300. <= hls_values['hue'] < 360.:
+        _r, _g, _b = c, 0, x
+
+    rbg = {
+        'red': (_r + m) * 255,
+        'green': (_g + m) * 255,
+        'blue': (_b + m) * 255
+    }
+    return rbg
 
 
 def color_conversion():
@@ -65,4 +83,11 @@ def color_conversion():
 
 
 test_color = {'red': 0, 'green': 0, 'blue': 255}
-print(rbg_to_hls(test_color))
+hls = rbg_to_hls(test_color)
+print(hls)
+print(hls_to_rbg(hls))
+
+# Point = namedtuple('Point', 'x y')
+# p2 = Point(100, 100)
+# p1 = Point(120, 600)
+# print(draw_line(p1, p2))
