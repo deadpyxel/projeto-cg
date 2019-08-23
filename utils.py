99e81d950dcc78584e900f27256c85fbf7b850db
deadpyxel <ghost0092@gmail.com>
2019-08-23 14:28:19 -0300

    feat: Add RGB to HLS conversion

--- utils.py
+++ utils.py
@@ -1,5 +1,10 @@
 from collections import namedtuple
 
+RBGMAX = 255
+HLSMAX = [360, 100., 100.]
+
+# https://support.microsoft.com/pt-br/help/29240/how-to-converting-colors-between-rgb-and-hls-hbs
+
 
 def draw_line(p1, p2):
     dx = p2.x - p1.x
@@ -23,10 +28,41 @@ p2 = Point(100, 100)
 p1 = Point(120, 600)
 print(draw_line(p1, p2))
 
-def rbg_to_hsl():
 
-def hsl_to_rbg():
+def rbg_to_hls(rbg_values: dict):
+    # https://www.rapidtables.com/convert/color/rgb-to-hsl.html
+    _r = rbg_values['red'] / RBGMAX
+    _g = rbg_values['green'] / RBGMAX
+    _b = rbg_values['blue'] / RBGMAX
+
+    c_max = max(_r, _g, _b)
+    c_min = min(_r, _g, _b)
+    delta = c_max - c_min
+
+    lightness = (c_max + c_min) / 2
+    hue = 0.
+    saturation = 0.
+
+    if delta != 0.:
+        saturation = delta / (1 - abs(2 * lightness - 1))
+
+    if c_max == _r:
+        hue = 60 * (((_g - _b) / delta) % 6)
+    elif c_max == _g:
+        hue = 60 * (((_b - _r) / delta) + 2.)
+    elif c_max == _b:
+        hue = 60 * (((_r - _g) / delta) + 4.)
+
+    return {'hue': hue, 'lightness': lightness, 'saturation': saturation}
+
+
+def hls_to_rbg():
     pass
+
 
 def color_conversion():
     pass
+
+
+test_color = {'red': 0, 'green': 0, 'blue': 255}
+print(rbg_to_hls(test_color))
